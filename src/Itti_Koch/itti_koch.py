705109e9d67c19fd660cd57ee1817c476fa325de
Redwanul Haque Sourave <redwanul.sourav@gmail.com>
2025-05-13 18:07:21 -0400

    Pytorch conversion

--- src/Itti_Koch/itti_koch.py
+++ src/Itti_Koch/itti_koch.py
@@ -1,30 +1,56 @@
 import numpy as np
 import cv2
 from scipy.signal import convolve2d
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
 # Implementation for Itti-Koch model.
 
 class IttiKochModel():
+    """
+        IMPORTANT: Use cv2.BORDER_DEFAULT / cv2.BORDER_REFLECT_101 for everything
+    """
     def __init__(self):
-        pass
+        self.dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def gaussianBlur(self, img, kernelSize = 5, sigma = 1.0):
+        """
+            Perform a gaussian blur
+            Input:
+                img: torch.Tensor -> img.shape: (1, 1, H, W)
+            Returns:
+                result: torch.Tensor -> result.shape: (1, 1, H, W)
+        """
+
+        channels = img.shape[1]
+        x = torch.arange(kernelSize).float() - kernelSize // 2
+        gauss = torch.exp(-x**2 / (2 * sigma ** 2))
+        kernel1d = gauss / gauss.sum()
+        kernel2d = kernel1d[:, None] * kernel1d[None, :]    # (5, 5)
+        kernel2d = kernel2d.unsqueeze(0).unsqueeze(0).to(self.dev)    # (1, 1, 5, 5)
         
-    def downsample(self, img: np.ndarray):
-        img = cv2.GaussianBlur(img, (3,3), 2)
-        newDim = (img.shape[1] // 2, img.shape[0] // 2)
-        downsampled = cv2.resize(img, newDim)
-        return downsampled
+        padding = kernelSize // 2
+        paddedImg = F.pad(img, (padding, padding, padding, padding), mode = 'reflect')
+        return F.conv2d(img, kernel2d)
+
+    def pyrDown(self, img):
+        blurred = self.gaussianBlur(img, kernelSize = 5, sigma = 1.0)
+        return F.interpolate(img, scale_factor = 0.5, mode = 'bilinear', align_corners = False)
+
     
-    def gaussianPyramid(self, img: np.ndarray):
+    def gaussianPyramid(self, img):
         result = {}
         result[0] = img 
         for level in list(range(1,9)):
-            result[level] = self.downsample(result[level-1])
+            result[level] = self.pyrDown(result[level-1])
 
         return result
 
-    def extractVisualFeatures(self, img: np.ndarray):
-        r = img[:, :, 2].astype(np.float64)
-        g = img[:, :, 1].astype(np.float64)
-        b = img[:, :, 0].astype(np.float64)
+    def extractVisualFeatures(self, img):
+        r = img[0, 2, :, :].unsqueeze(0).unsqueeze(0)
+        g = img[0, 1, :, :].unsqueeze(0).unsqueeze(0)
+        b = img[0, 0, :, :].unsqueeze(0).unsqueeze(0)
+        # print(r.shape)
         intensity = (r + g + b) / 3
         
         maxI = intensity.max()
@@ -38,7 +64,7 @@ class IttiKochModel():
         red = (r - (g + b) / 2)
         green = (g - (r + b) / 2)
         blue = (b - (r + g) / 2)
-        yellow = (r + g) / 2 - np.abs(r - g) / 2 - b
+        yellow = (r + g) / 2 - torch.abs(r - g) / 2 - b
         
         intensity[intensity < 0] = 0
         red[red < 0] = 0
@@ -63,8 +89,8 @@ class IttiKochModel():
 
     def upscaleImage(self, img, coarserScale: int, finerScale: int):
         scaleDiff = coarserScale - finerScale
-        upscaleDim = (img.shape[1] * (1 << scaleDiff), img.shape[0] * (1 << scaleDiff))
-        upscaled = cv2.resize(img, upscaleDim)
+        upscaleDim = (img.shape[3] * (1 << scaleDiff), img.shape[2] * (1 << scaleDiff))
+        upscaled = F.interpolate(img, upscaleDim, mode = 'bilinear')
         return upscaled
 
     def acrossScaleDiff(self, pyramids: dict):
@@ -103,28 +129,28 @@ class IttiKochModel():
                 _s = i + j  # The coarser scale.
 
                 tempI = self.upscaleImage(iSigma[_s], _s, i)    # Upscale the coarser scaled image, to finer scale.
-                tempI = np.abs(iSigma[i] - tempI)   # Take the absolute difference.
+                tempI = torch.abs(iSigma[i] - tempI)   # Take the absolute difference.
                 iCS[i][_s] = tempI    # Store.
                 # Calculate BY(c,s)
                 
                 # upscaled_B_S = self.upscaleImage(BSigma[_s], _s, i) # Upscale the coarser scaled image, to finer scale.
                 # upscaled_Y_S = self.upscaleImage(YSigma[_s], _s, i)    # Upscale the coarser scaled image, to finer scale.
-                diff = np.abs(ySigma[_s] - bSigma[_s])
-                temp = np.abs((bSigma[i] - ySigma[i]) - self.upscaleImage(diff, _s, i))  # Take the absolute difference.
+                diff = torch.abs(ySigma[_s] - bSigma[_s])
+                temp = torch.abs((bSigma[i] - ySigma[i]) - self.upscaleImage(diff, _s, i))  # Take the absolute difference.
                 byCS[i][_s] = temp    # Store.
 
                 # upscaled_G_s = self.upscaleImage(GSigma[_s], _s, i)   
                 # upscaled_R_s = self.upscaleImage(RSigma[_s], _s, i)
-                diff = np.abs(gSigma[_s] - rSigma[_s])
-                temp = np.abs((rSigma[i] - gSigma[i]) - self.upscaleImage(diff, _s,i))
+                diff = torch.abs(gSigma[_s] - rSigma[_s])
+                temp = torch.abs((rSigma[i] - gSigma[i]) - self.upscaleImage(diff, _s,i))
                 rgCS[i][_s] = temp
 
                 angles = list(oSigmaTheta[list(oSigmaTheta.keys())[0]].keys())  # Retrieve the list of angles
                 
                 oCSTheta[i][_s] = {}
                 for k in angles:
-                    upscaled_Os_Theta = self.upscaleImage(oSigmaTheta[_s][k], _s, i).astype(np.float64)   # Upscale
-                    oCSTheta[i][_s][k] = np.abs(oSigmaTheta[i][k].astype(np.float64) - upscaled_Os_Theta)   # Absolute Diff
+                    upscaled_Os_Theta = self.upscaleImage(oSigmaTheta[_s][k], _s, i)   # Upscale
+                    oCSTheta[i][_s][k] = torch.abs(oSigmaTheta[i][k] - upscaled_Os_Theta)   # Absolute Diff
         
         
         result = {}
@@ -163,39 +189,53 @@ class IttiKochModel():
 
         return result
 
-    def fsdLaplacian(self, img: np.ndarray, n: int):
-        lpf = np.zeros((5,1), dtype=np.float64)
-        lpf[2, 0] = 3.0/8.0      # Middle
-        lpf[1, 0] = 0.25
-        lpf[3, 0] = 0.25
-        lpf[0, 0] = 1.0/16
-        lpf[4, 0] = 1.0/16
-        lpf = np.matmul(lpf, lpf.transpose())
+    def fsdLaplacian(self, img, n: int):
+        lpf = torch.zeros(5).float().to(self.dev)
+        lpf[2] = 3.0/8.0      # Middle
+        lpf[1] = 0.25
+        lpf[3] = 0.25
+        lpf[0] = 1.0/16
+        lpf[4] = 1.0/16
+        
+        lpf = lpf[:, None] * lpf[None, :]
+        lpf = lpf.unsqueeze(0).unsqueeze(0)
+        # print(lpf)
+        # lpf = np.matmul(lpf, lpf.transpose())
 
         fsdLowPassedPyr = {}
         fsdLaplacianPyr = {}
         fsdLowPassedPyr[0] = img   # At scale 0, we have the original image.
 
         for i in range(1, n+1):
-            g0 = cv2.filter2D(fsdLowPassedPyr[i-1], -1, lpf, borderType = cv2.BORDER_REFLECT)
+            # padding = kernelSize // 2
+            # paddedImg = 
+            g0 = F.conv2d(F.pad(fsdLowPassedPyr[i-1], (2, 2, 2, 2), mode = 'reflect'), lpf)
             fsdLaplacianPyr[i-1] = fsdLowPassedPyr[i-1] - g0  # The difference is the laplacian at previous scale.
-            newDim = (fsdLaplacianPyr[i-1].shape[1] // 2, fsdLaplacianPyr[i-1].shape[0] // 2) # The gaussian at current scale will be downsampled to this dim.
-            fsdLowPassedPyr[i] = cv2.resize(g0, newDim)   # Downsample and store.
+            newDim = (fsdLaplacianPyr[i-1].shape[3] // 2, fsdLaplacianPyr[i-1].shape[2] // 2) # The gaussian at current scale will be downsampled to this dim.
+            fsdLowPassedPyr[i] = F.interpolate(g0, newDim)   # Downsample and store.
+            # print(g0.shape)
+            # print(fsdLowPassedPyr[i].shape)
         
         return (fsdLowPassedPyr, fsdLaplacianPyr)
 
-    def orientedGaborPyramid(self, img: np.ndarray, anglesN, pyramidDepth): 
-        lpf = np.zeros((5,1), dtype=np.float64)
-        lpf[2, 0] = 3.0/8.0      # Middle
-        lpf[1, 0] = 0.25
-        lpf[3, 0] = 0.25
-        lpf[0, 0] = 1.0/16.0
-        lpf[4, 0] = 1.0/16.0
-        lpf = np.matmul(lpf, lpf.transpose())
+    def orientedGaborPyramid(self, img, anglesN, pyramidDepth): 
+        lpf = torch.zeros(5).float().to(self.dev)
+        lpf[2] = 3.0/8.0      # Middle
+        lpf[1] = 0.25
+        lpf[3] = 0.25
+        lpf[0] = 1.0/16.0
+        lpf[4] = 1.0/16.0
+        lpf = lpf[:, None] * lpf[None, :]
+        lpf = lpf.unsqueeze(0).unsqueeze(0)
+        # lpf = torch.complex(lpf, lpf)
+        # print(lpf)
+        # print(lpf.shape)
+        # lpf = np.matmul(lpf, lpf.transpose())
         
-        r = img[:, :, 2].astype(np.float64)
-        g = img[:, :, 1].astype(np.float64)
-        b = img[:, :, 0].astype(np.float64)
+        r = img[0, 2, :, :].unsqueeze(0).unsqueeze(0)
+        g = img[0, 1, :, :].unsqueeze(0).unsqueeze(0)
+        b = img[0, 0, :, :].unsqueeze(0).unsqueeze(0)
+
         intensity = (r + g + b) / 3
 
         orientedFeatures = {}
@@ -205,41 +245,52 @@ class IttiKochModel():
         for p, img in laplacian.items():
             orientedFeatures[p] = {}
             for alpha in range(1, anglesN + 1):
-                imgI = img.astype(np.complex128)
-                H, W = img.shape[0], img.shape[1]
-                xx = np.arange(W) - W // 2
-                yy = np.arange(H) - H // 2
-                X, Y = np.meshgrid(xx, yy)
+                # imgI = img.astype(np.complex128)
+                H, W = img.shape[2], img.shape[3]
+                xx = torch.arange(W) - W // 2
+                yy = torch.arange(H) - H // 2
+                X, Y = torch.meshgrid(xx, yy)
 
-                theta = np.pi / 4 * (alpha - 1)
-                k = (np.pi / 2) * np.array([np.cos(theta), np.sin(theta)])
-                multiplier = k[0] * X + k[1] * Y
-                imgI = img * np.exp(1j * multiplier)
-                convolved = convolve2d(imgI, lpf, mode='same', boundary= 'symm')
-                imgM = np.abs(convolved)
+                theta = torch.pi / 4 * (alpha - 1)
+                k = (torch.pi / 2) * torch.tensor([torch.cos(torch.tensor(theta)), torch.sin(torch.tensor(theta))])
+                multiplier = torch.complex(torch.zeros(img.shape[2], img.shape[3]), k[0] * X + k[1] * Y)
+                multiplier = torch.exp(multiplier).to(self.dev).unsqueeze(0).unsqueeze(0)
+                # print(type(multiplier))
+                # print(multiplier.dtype)
+                # print(multiplier.shape) # (H, W)
+                imgI = img * multiplier
+                # F.pad(img, (padding, padding, padding, padding)
+                imgI = F.pad(imgI, (2, 2, 2, 2), mode = 'reflect')
+                real = imgI.real
+                imag = imgI.imag
+                convolved = torch.complex(F.conv2d(real, lpf), F.conv2d(imag, lpf))
+                # convolved = F.conv2d(imgI, lpf)
+                imgM = torch.abs(convolved)
+                # print(imgM.shape)
                 orientedFeatures[p][alpha] = imgM
         return orientedFeatures
     
-    def mapNorm(self, img: np.ndarray):
+    def mapNorm(self, img):
         for i in range(10):
-            minValue = np.min(img)
-            maxValue = np.max(img)
+            minValue = img.min()
+            maxValue = img.max()
             img = (img - minValue) / (maxValue - minValue)
-            kernel_size = (151, 151)    # The paper mentions a "big" kernel.
-            xx = np.arange(kernel_size[1]) - kernel_size[1] // 2
-            yy = np.arange(kernel_size[0]) - kernel_size[0] // 2
+            kernel_size = (75, 75)    # The paper mentions a "big" kernel.
+            xx = torch.arange(kernel_size[1]) - kernel_size[1] // 2
+            yy = torch.arange(kernel_size[0]) - kernel_size[0] // 2
 
-            X, Y = np.meshgrid(xx, yy)
-            pwr = (-np.square(X) - np.square(Y))/(2 * 0.02 * img.shape[1])
+            X, Y = torch.meshgrid(xx, yy)
+            pwr = (-torch.square(X) - torch.square(Y))/(2 * 0.02 * img.shape[3])
 
-            gauss_1 = (np.exp(pwr) * (0.5) * (0.5))/(2 * np.pi * np.square(0.02*img.shape[1]))
+            gauss_1 = (torch.exp(pwr) * (0.5) * (0.5))/(2 * torch.pi * torch.square(torch.tensor(0.02 * img.shape[3])))
             # X, Y = np.meshgrid(xx, yy)
-            pwr = (-np.square(X) - np.square(Y))/(2 * 0.25 * img.shape[1])
-            gauss_2 = (np.exp(pwr) * (1.5) * (1.5))/(2 * np.pi * np.square(0.25*img.shape[1]))
+            pwr = (-torch.square(X) - torch.square(Y))/(2 * 0.25 * img.shape[3])
+            gauss_2 = (torch.exp(pwr) * (1.5) * (1.5))/(2 * torch.pi * torch.square(torch.tensor(0.25*img.shape[3])))
 
-            dogFilter = gauss_1 - gauss_2
-            tempImg = img.copy()
-            img = tempImg + cv2.filter2D(img, -1, dogFilter, borderType = cv2.BORDER_REFLECT) - 0.02
+            dogFilter = (gauss_1 - gauss_2).unsqueeze(0).unsqueeze(0).to(self.dev)
+            # tempImg = img.copy()
+            img = img + F.conv2d(F.pad(img, (kernel_size[1] // 2, kernel_size[1] // 2, kernel_size[0] // 2, kernel_size[0] // 2), mode = 'reflect'), 
+            dogFilter) - 0.02
             img[img < 0] = 0
         # minValue = np.min(img)
         # maxValue = np.max(img)
@@ -279,8 +330,8 @@ class IttiKochModel():
             for s, img in tempDict.items(): # Loop over scale scales.
                 img = self.mapNorm(img)
                 if c < 4:
-                    newDim = (img.shape[1] // (1 << (4 - c)), img.shape[0] // (1 << (4 - c)))
-                    img = cv2.resize(img, newDim)
+                    newDim = (img.shape[3] // (1 << (4 - c)), img.shape[2] // (1 << (4 - c)))
+                    img = F.interpolate(img, newDim, mode = 'bilinear', align_corners = False)
                 
                 assert c <= 4
 
@@ -301,8 +352,8 @@ class IttiKochModel():
                 RGMap = self.mapNorm(RG_C_S[c][s])
                 img = BYMap + RGMap
                 if c < 4:
-                    newDim = (img.shape[1] // (1 << (4 - c)), img.shape[0] // (1 << (4 - c)))
-                    img = cv2.resize(img, newDim)
+                    newDim = (img.shape[3] // (1 << (4 - c)), img.shape[2] // (1 << (4 - c)))
+                    img = F.interpolate(img, newDim, mode = 'bilinear', align_corners = False)
             
                 assert c <= 4
 
@@ -325,8 +376,8 @@ class IttiKochModel():
                     img = self.mapNorm(O_C_S_Theta[c][s][angle])
 
                     if c < 4:
-                        newDim = (img.shape[1] // (1 << (4 - c)), img.shape[0] // (1 << (4 - c)))
-                        img = cv2.resize(img, newDim)
+                        newDim = (img.shape[3] // (1 << (4 - c)), img.shape[2] // (1 << (4 - c)))
+                        img = F.interpolate(img, newDim, mode = 'bilinear', align_corners = False)
             
                     assert c <= 4
 
@@ -350,39 +401,51 @@ class IttiKochModel():
         return (img - imgMin)  / (imgMax - imgMin)
 
     def saliencyMap(self, img: np.ndarray):
-        img = img.astype(np.float32)/255.0
+        img = torch.from_numpy(img).float().to(self.dev)
+        img = img / 255.0
+        img = img.unsqueeze(0)  # (1, H, W, 3)
+        img = img.permute(0, 3, 1, 2)   # (1, 3, H, W)
+        print(img.shape)
         pyramids = self.extractVisualFeatures(img)
         orientation = self.orientedGaborPyramid(img, 4, 9)        
-        
         pyramids['orientation_pyr'] = orientation
-
-        # for scale, pyr in orientation.items():
-        #     print(np.max(orientation[scale][1] - orientation[scale][2]))
-        #     for angle, img in pyr.items():
-        #         cv2.imwrite(f'{scale}-{angle}.jpg', self.minMaxNormalize(img)*255)
-
-        result = {}
-        result['pyramids'] = pyramids
-        
         pyramids = self.acrossScaleDiff(pyramids)
-        result['merged_pyramids'] = pyramids
-
         IBar = self.mergeIntensityMaps(pyramids['I_C_S'])
         CBar = self.mergeColorMaps(pyramids['BY_C_S'], pyramids['RG_C_S'])
         OBar = self.mergeOrientationMaps(pyramids['O_C_S_Theta'])
-        result['I_Bar'] = IBar
-        result['C_Bar'] = CBar
-        result['O_Bar'] = OBar
-        # cv2.imwrite('IBar.jpg', (self.minMaxNormalize(IBar)*255).astype(np.uint8))
-        # cv2.imwrite('CBar.jpg', (self.minMaxNormalize(CBar)*255).astype(np.uint8))
-        # cv2.imwrite('OBar.jpg', (self.minMaxNormalize(OBar)*255).astype(np.uint8))
-        
-        # print(OBar.shape)
-        # cv2.imwrite('obar.jpg', OBar)
         sMap = (IBar + CBar + OBar) 
-        # sMap = cv2.normalize(sMap, sMap, 0, 255, cv2.NORM_MINMAX)
-        sMapMin = np.min(sMap)
-        sMapMax = np.max(sMap)
+        sMapMin = sMap.min()
+        sMapMax = sMap.max()
         sMap = (sMap - sMapMin) / (sMapMax - sMapMin)
-        return sMap * 255
+        sMap = sMap.permute(0, 2, 3, 1).squeeze(0)
+        sMap = sMap.detach().cpu().numpy()
+        sMap = (sMap * 255).astype(np.uint8)
+        return sMap
+
+        # img = img.astype(np.float32)/255.0
+        # pyramids = self.extractVisualFeatures(img)
+        
+
+        # # for scale, pyr in orientation.items():
+        # #     print(np.max(orientation[scale][1] - orientation[scale][2]))
+        # #     for angle, img in pyr.items():
+        # #         cv2.imwrite(f'{scale}-{angle}.jpg', self.minMaxNormalize(img)*255)
+
+        # result = {}
+        # result['pyramids'] = pyramids
+        
+        # result['merged_pyramids'] = pyramids
+
+        # result['I_Bar'] = IBar
+        # result['C_Bar'] = CBar
+        # result['O_Bar'] = OBar
+        # # cv2.imwrite('IBar.jpg', (self.minMaxNormalize(IBar)*255).astype(np.uint8))
+        # # cv2.imwrite('CBar.jpg', (self.minMaxNormalize(CBar)*255).astype(np.uint8))
+        # # cv2.imwrite('OBar.jpg', (self.minMaxNormalize(OBar)*255).astype(np.uint8))
+        
+        # # print(OBar.shape)
+        # # cv2.imwrite('obar.jpg', OBar)
+        # # sMap = cv2.normalize(sMap, sMap, 0, 255, cv2.NORM_MINMAX)
+        # sMapMax = np.max(sMap)
+        # return sMap * 255
 
--- src/Itti_Koch/main.py
+++ src/Itti_Koch/main.py
@@ -3,21 +3,21 @@ from itti_koch import IttiKochModel
 import numpy as np
 
 model = IttiKochModel()
-img = cv2.imread('STIMautobahn/DBtest/cB10_O012.ppm')
-expected = cv2.imread('STIMautobahn/DBtest/cB10_K027.ppm')
+img = cv2.imread('../shreelock-gbvs/images/4.jpg')
+# expected = cv2.imread('STIMautobahn/DBtest/cB10_K027.ppm')
 img = cv2.resize(img, (1024, 1024))
-cv2.imwrite('origina.jpg', img)
-cv2.imwrite('expected.jpg', expected)
 smap = model.saliencyMap(img)
-smap = smap.astype(np.uint8)
-smap2 = cv2.resize(smap, (1024, 1024))
-smap3 = smap2.astype(np.float64)/255.0
-out3 = img.copy()
-out3[:,:,0] = out3[:, :, 0] * smap3
-out3[:,:,1] = out3[:, :, 1] * smap3
-out3[:,:,2] = out3[:, :, 2] * smap3
+print(smap.shape)
+cv2.imwrite('outsmap.jpg', smap)
+# smap = smap.astype(np.uint8)
+# smap2 = cv2.resize(smap, (1024, 1024))
+# smap3 = smap2.astype(np.float64)/255.0
+# out3 = img.copy()
+# out3[:,:,0] = out3[:, :, 0] * smap3
+# out3[:,:,1] = out3[:, :, 1] * smap3
+# out3[:,:,2] = out3[:, :, 2] * smap3
 
-# out3 = (img * smap3).astype(np.uint8)
+# # out3 = (img * smap3).astype(np.uint8)
 
-cv2.imwrite('smap.jpg', smap2)
-cv2.imwrite('out3.jpg', out3)
+# cv2.imwrite('smap.jpg', smap2)
+# cv2.imwrite('out3.jpg', out3)
