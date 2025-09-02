cc87504a1371e91fa2abadf9edc23381e8b22cbf
Redwanul Haque Sourave <redwanul.sourav@gmail.com>
2025-09-02 02:54:39 -0400

    vivit model update

--- src/vivit/model.py
+++ src/vivit/model.py
@@ -10,12 +10,15 @@ class MultiHeadAttention(nn.Module):
         self.h = h
         self.dropout = nn.Dropout(p = dropout)
         self.d_v = d_v
-       return torch.matmul(p_attn, value), p_attn
+        self.W_q = nn.Linear(in_features = d_model, out_features = h * d_k)
+        self.W_k = nn.Linear(in_features = d_model, out_features = h * d_k)
+        self.W_v = nn.Linear(in_features = d_model, out_features = h * d_v)
+        self.W_o = nn.Linear(in_features = h * d_v, out_features = d_model)
 
     def forward(self, x):
         B, T, F = x.shape
-        query_embed = self.W_q(x)   # B, T, d_model
-        key_embed = self.W_k(x)     # B, T, d_model
+        query_embed = self.W_q(x)   # B, T, h * d_k
+        key_embed = self.W_k(x)     # B, T, h * d_k
         value_embed = self.W_v(x)   # B, T, h * d_v
 
         # Split into `h` heads
@@ -28,11 +31,14 @@ class MultiHeadAttention(nn.Module):
 
         # Calculate weights.
         attn_weights = torch.matmul(query, key.transpose(-2, -1))   # B, h, T, T
-        attn_weights = attn_weights / torch.sqrt(d_k)
+        attn_weights = attn_weights / torch.sqrt(d_k) # B, h, T, T
 
-        attn_weights = attn_weights.softmax(dim = -1)
+        attn_weights = attn_weights.softmax(dim = -1) # B, h, T, T
 
-        encodings = torch.matmul(attn_weights, value)
+        encodings = torch.matmul(attn_weights, value) # B, h, T, d_v
+        encodings = encodings.permute(0, 2, 1, 3) # B, T, h, d_v
+        encodings = encodings.view(0, 1, -1); # B, T, h * d_v
+        encodings = self.w_o(encodings) # B, T, d_model
         return encodings
         
 class LayerNorm(nn.Module):
@@ -71,11 +77,15 @@ class TransformerEncoder(nn.Module):
 
     def forward(self, x):
         B, N, embed_dim = x.shape
-        x = self.multi_head_attention(x) + x
-        x = self.layer_norm0(x)
-        x = self.feed_forward(x) + x
-        x = self.layer_norm1(x)
-        return x
+        x = self.multi_head_attention(x) + x  # B, N, embed_dim
+        x = self.layer_norm0(x) # B, N, embed_dim
+        x = self.feed_forward(x) + x  # B, N, embed_dim
+        x = self.layer_norm1(x) # B, N, embed_dim
+        return x # B, N, embed_dim
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, embed_dim):
+        self.embed_dim = embed_dim
 
 
 class SMapEncoder(nn.Module):
@@ -104,7 +114,7 @@ class SMapDecoder(nn.Module):
         
 class ViViT(nn.Module):
     def __init__(self, embed_dim, p, c = 3, t = 32, h = 64, w = 64):
-        super(ViVit, self).__init__()
+        super(ViViT, self).__init__()
         self.p = p
         self.c = c
         self.t = t
@@ -117,19 +127,19 @@ class ViViT(nn.Module):
     def forward(self, x):
         B, T, C, H, W = x.shape
 
-        x = x.reshape(B, T, C, H // self.p, self.p, W // self.p, self.p)   # (B, T, C, H // P, P, W // P, P)
-        x = x.permute(0, 1, 2, 3, 5, 4, 6)
-        x = x.reshape(B, T, C, (H // self.p) * (W // self.p), P, P)    # (B, T, (H*W)/(P^2), P, P)
+        x = x.view(B, T, C, H // self.p, self.p, W // self.p, self.p)   # (B, T, C, H // P, P, W // P, P)
+        x = x.permute(0, 1, 2, 3, 5, 4, 6) # (B, T, C, H // self.p, W // self.p, p, p)
+        x = x.view(B, T, C, (H // self.p) * (W // self.p), P, P)    # (B, T, C, (H*W)/(P^2), P, P)
         x = x.permute(0, 1, 3, 2, 4, 5)  # (B, T, N, C, P, P)
-        x = x.reshape(B, T, (H // self.p) * (W // self.p), -1)   # (B, T, (H*W)/(P^2), C * P ^ 2)
-        x = x.permute(0, 2, 1, 3) 
-        x = x.reshape(B, T * (H // self.p) * (W // self.p), -1) # (B, T * N, C * P^2)
+        x = x.view(B, T, (H // self.p) * (W // self.p), -1)   # (B, T, N, C * P ^ 2)
+        x = x.permute(0, 2, 1, 3) # (B, N, T, C * P^2)
+        x = x.view(B, T * (H // self.p) * (W // self.p), -1) # (B, T * N, C * P^2)
         # N = (H * W) / (P ^ 2)
         x = self.linear(x)  # (B, T * N, embed_dim)
         # TODO: Put the target encoding here.
         x = x + self.position_embedding         # (B, T * N, embed_dim)
 
-        x = self.transformer_encoders(x)
+        x = self.transformer_encoders(x) # (B, T * N, embed_dim)
 
 
 
